9fec7b210837eef8f3e47bcad71a7f1363a5f8ff
wooyoung0407 <wooyoung0407@naver.com>
2023-07-07 09:14:36 +0900

    230707test

---
+++ src/test/java/com/example/test2/Test2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.test2;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class Test2ApplicationTests {
+
+	@Test//db에 넣기위한 test 작업
+	void testJpa() {
+
+}
